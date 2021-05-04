99f0790958dd935b5abc284a10fd05d194d05429
re098262 <re098262@kentaku.co.jp>
2021-05-04 12:51:51 +0900

    sql:problemNo_1757

---
+++ src/lc_java_1108.java
@@ -0,0 +1,27 @@
+public class lc_java_1108 {
+    public static void main(String[] args) {
+        //ipAddrを定義する
+        String ipAddr = "123.1.1.1";
+        System.out.println(defangIPaddr(ipAddr));
+        ipAddr = "255.100.50.0";
+        System.out.println(defangIPaddr(ipAddr));
+        ipAddr ="227.222.40.237";
+        System.out.println(defangIPaddr(ipAddr));
+    }
+
+    public static String defangIPaddr(String address) {
+        StringBuilder newAddr = new StringBuilder();
+        //for ループで処理する
+        for (int i = 0; i < address.length(); i++) {
+            int spos = address.indexOf('.', i);
+            if (spos > 0 && i != spos) {
+                newAddr.append(address, i, spos).append("[.]");
+                i = spos;
+            } else if (spos < 0) {
+                newAddr.append(address.substring(i));
+                break;
+            }
+        }
+        return newAddr.toString();
+    }
+}
