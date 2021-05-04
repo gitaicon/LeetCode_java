31c6f2b9a571c0560ee3be540160ea7d951ccc60
lijiayujijinhui <lijiayujijinhui@yahoo.co.jp>
2021-05-05 01:21:12 +0900

    java:problemNo_760_bugfix

--- src/lc_java_760.java
+++ src/lc_java_760.java
@@ -3,16 +3,22 @@ import java.util.Arrays;
 public class lc_java_760 {
     public static void main(String[] args) {
         int[] results;
-        // Your input
+        // Your input1
         int[] testArr1 = {12, 28, 46, 32, 50};
         int[] testArr2 = {50, 12, 32, 46, 28};
-        // Expected：[1,4,3,2,0]
+        // Expected1：[1,4,3,2,0]
+        results = anagramMappings(testArr1, testArr2);
+        System.out.println(Arrays.toString(results));
+        // Your input2
+        testArr1 = new int[]{84,46};
+        testArr2 = new int[]{84,46};
+        // Expected2：[0,1]
         results = anagramMappings(testArr1, testArr2);
         System.out.println(Arrays.toString(results));
     }
 
     public static int[] anagramMappings(int[] A, int[] B) {
-        int[] rtnArr = new int[5];
+        int[] rtnArr = new int[A.length];
         //配列Ａを検索する
         for (int i = 0; i < A.length; i++) {
             //配列Ｂを検索する
